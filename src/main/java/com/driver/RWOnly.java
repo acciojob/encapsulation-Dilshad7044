2ee0d664a1ae0b01bd823f9bef88a6dcc746f4da
DILSHAD ANSARI <84556373+Dilshad7044@users.noreply.github.com>
2024-09-16 18:56:47 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    // Task 2: Private member variable
+    private String name;
+
+    // Task 5: Getter and Setter methods for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
